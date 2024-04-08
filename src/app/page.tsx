7fb40dd622f3186aebd0619efcd8e74b5fbd9435
Preyanshu <mishrapreyanshu@gmail.com>
2024-04-08 22:13:51 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/app/page.tsx 	new file:   src/components/taskdetailscard.tsx 	new file:   src/components/teamMemberCard.tsx 	new file:   src/components/teamScoreCard.tsx 	new file:   src/components/ui/button.tsx 	new file:   src/components/ui/checkbox.tsx 	new file:   src/components/ui/dropdown-menu.tsx 	new file:   src/components/ui/input.tsx 	new file:   src/components/ui/switch.tsx 	new file:   src/components/ui/table.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
 import Image from "next/image";
+import {TeamMemberCard} from "../components/teamMemberCard";
+import { TeamScoreCard } from "@/components/teamScoreCard";
+// import { TeamDetailsCard } from "@/components/taskdetailscard";
+import { Column } from "@tanstack/react-table";
 
 export default function Home() {
   return (
     <main className="bg-white">
-      <h1>Team Name</h1>
+      <div className="">
+      <h1 className="">Team Name</h1>
+      <TeamScoreCard />
+      
+      <TeamMemberCard />
+
+      </div>
+      
 
     
     
